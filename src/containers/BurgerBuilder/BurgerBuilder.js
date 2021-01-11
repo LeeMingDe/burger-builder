1bb188fb24c9e16e6cbc5ca4568d62b359be9764
Lee Ming De <leemingde@gmail.com>
2021-01-11 22:50:48 +0800

    Complete authentication

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,7 +30,12 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    this.setState({purchasing: true})
+    if (this.props.isAuthenticated) {
+      this.setState({purchasing: true})
+    } else {
+      this.props.onSetAuthRedirectPath('/checkout');
+      this.props.history.push('/auth');
+    }
   }
 
   purchaseCancelHander = () => {
@@ -67,6 +72,7 @@ class BurgerBuilder extends Component {
             disabledInfo = {disabledInfo}
             purchasable = {this.updatePurchaseState(this.props.ings)}
             price = {this.props.price}
+            isAuth = {this.props.isAuthenticated}
             ordered = {this.purchaseHandler}/>
         </Aux>
       );
@@ -95,7 +101,8 @@ const mapStateToProps = state => {
   return {
     ings: state.burgerBuilder.ingredients,
     price: state.burgerBuilder.totalPrice,
-    error: state.burgerBuilder.error
+    error: state.burgerBuilder.error,
+    isAuthenticated: state.auth.token !== null
   };
 }
 
@@ -104,7 +111,8 @@ const mapDispatchToProps = dispatch => {
     onIngredientAdded: (ingName) => dispatch(actions.addIngredient(ingName)),
     onIngredientRemoved: (ingName) => dispatch(actions.removeIngredient(ingName)),
     onInitIngredients: () => dispatch(actions.initIngredients()),
-    onInitPurchase: () => dispatch(actions.purchaseInit())
+    onInitPurchase: () => dispatch(actions.purchaseInit()),
+    onSetAuthRedirectPath: path => dispatch(actions.setAuthRedirectPath(path))
   };
 }
 
